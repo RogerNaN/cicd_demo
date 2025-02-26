63580b96d01df86275f86125b35f7e0207947750
RogerNaN <00857002@email.ntou.edu.tw>
2025-02-26 16:33:25 +0800

    fix: test path

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,9 @@
 import pytest
-from app.main import app
+import sys
+import os
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from app.main import app
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
